6e6f78560bf3018817535a0e6128f0d229a3c228
mikessh <mikhail.shugay@gmail.com>
2017-11-11 15:30:18 +0300

    #1: some cleanup

--- src/main/java/com/antigenomics/pmem/representation/algebra/matrix/MatrixStorageIndex.java
+++
@@ -1,9 +0,0 @@
-package com.antigenomics.pmem.representation.algebra.matrix;
-
-public interface MatrixStorageIndex {
-    int getIndex(int i, int j);
-
-    int getNumberOfRows();
-
-    int getNumberOfColumns();
-}
